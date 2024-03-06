7bdbe2b77ad38c5468b2c638326c5c068a3627c8
vladislav <11332266vlados@gmail.com>
2024-03-06 16:40:57 +0300

    add css js htm images and fonts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.0015}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .0025}deg;
+		`
+    })
+})
+
+$(function() {
+    $.scrollify({
+        section : ".layer_scroll",
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,46 @@
-324345345345
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
+
+    <link rel="stylesheet" href="css/main.css">
+    <script src="libs/jq.js" defer></script>
+    <script src="libs/jquery.scrollify.js" defer></script>
+    <script src="js/app.js" defer></script>
+
+</head>
+<body>
+<div class="logo" style=""></div>
+
+<section class="layers layer_scroll">
+    <div class="layers__container">
+        <div class="layers__item layer-1" style="background-image: url('img/l-1.jpg')">
+        </div>
+        <div class="layers__item layer-2" style="background-image: url('img/l-2.png')"></div>
+        <div class="layers__item layer-3">
+            <div class="hero-content">
+                <h1>Sololeaver <span>Rocket League</span></h1>
+                <div class="hero-content__p">Стример, создатель контента и тренер</div>
+                <div class="button-start-wrap">
+                    <button class="button-start">Telegram</button>
+                    <button class="button-start">Boosty</button>
+                    <button class="button-start">YouTube</button>
+                    <button class="button-start">Discord</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="standart_section section_boosty layer_scroll">
+    <div class="standart_section__container">
+        <div><span  class="title">Спасибо бустерам</span></div>
+    </div>
+</section>
+
+</body>
+</html>
